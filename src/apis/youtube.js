287efe05e7eb523496999768d929c681791028f8
Ricardo <limaricardop@gmail.com>
2022-03-27 22:47:39 -0300

    App call back function && api request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,6 +4,7 @@ export default axios.Create ({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
+        type: 'video',
         maxResults: 5,
         key: process.env.YOUTUBE_KEY
     }
