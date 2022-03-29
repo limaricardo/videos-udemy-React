30775ba0b0493b967b9e9d392fd2531e5b527bbd
Ricardo <limaricardop@gmail.com>
2022-03-29 17:15:35 -0300

    Video List and its style

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from '../apis/youtube'
+import VideoList from "./VideoList";
 
 
 class App extends React.Component {
 
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         })
+        
+        this.setState({ videos : response.data.items })
     };
-
+    
+    
     
 
     render() {
-        console.log(process.env.REACT_APP_YOUTUBE_KEY)
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            <VideoList videos={this.state.videos} />
         </div>
         )
     }
